9917d26ce5f0e50353085d2d7cd006657f778e20
Z-thrones <zaviyar.ktk@gmail.com>
2026-04-01 17:16:05 +0500

    Add files via upload

---
+++ A26.py
@@ -0,0 +1,10 @@
+mean1 = 63
+wrong_number = 34
+correct_number = 64
+total_number = 161
+sum = mean1 * total_number
+print(sum)
+num2 = sum - wrong_number + correct_number
+print(num2)
+mean2 = num2 / total_number
+print("The correct mean is . ....." , mean2)
